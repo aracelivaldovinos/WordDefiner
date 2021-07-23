5575c47e7d3fb0187242b6ffe94165ce4573fb9a
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-23 16:23:42 -0500

    updating definition spec

--- spec/definitions_spec.rb
+++ spec/definitions_spec.rb
@@ -16,7 +16,7 @@ end
 
   describe('#save') do
     it("it will save definition in the @@definition hash") do 
-      definition = Definition.new({:definition => "easy", :id => @id})
+      definition = Definition.new({:definition => "without difficulty or effort", :id => @id})
       definition.save()
       expect(Definition.all).to(eq([definition]))
     end 
@@ -24,9 +24,9 @@ end
 
   describe('#==') do
     it("its the same definition if it has the same attributes") do 
-      definition = Definition.new({:definition => "easy", :id => @id})
+      definition = Definition.new({:definition => "without difficulty or effort", :id => @id})
       definition.save()
-      definition1 = Definition.new({:definition => "easy", :id => @id})
+      definition1 = Definition.new({:definition => "without difficulty or effort", :id => @id})
       definition.save()
       expect(definition).to(eq(definition1))
     end 
@@ -34,9 +34,9 @@ end
 
   describe('.clear') do
     it("it will clear definitions hash") do 
-      definition = Definition.new({:definition => "easy", :id => @id})
+      definition = Definition.new({:definition => "without difficulty or effort", :id => @id})
       definition.save()
-      definition1 = Definition.new({:definition => "easy", :id => @id})
+      definition1 = Definition.new({:definition => "without difficulty or effort", :id => @id})
       definition.save()
       Definition.clear()
       expect(Definition.all).to(eq([]))
@@ -46,9 +46,9 @@ end
   ## Need to work on find and update
   describe('.find') do
     it("it will find definition by id") do 
-      definition = Definition.new({:definition => "easy", :id => 1})
+      definition = Definition.new({:definition => "without difficulty or effort", :id => 1})
       definition.save()
-      definition1 = Definition.new({:definition => "hard", :id => 2})
+      definition1 = Definition.new({:definition => "free from worries or problems", :id => 2})
       definition.save()
       expect(Definition.find(definition.id)).to(eq(definition))
     end 
@@ -56,10 +56,10 @@ end
 
   describe('#update') do
     it("it will update definition by id") do 
-      definition = Definition.new({:definition => "easy", :id => 1})
+      definition = Definition.new({:definition => "without difficulty or effort", :id => 1})
       definition.save()
-      definition.update("hard")
-      expect(definition.definition()).to(eq("hard"))
+      definition.update("free from worries or problems")
+      expect(definition.definition()).to(eq("free from worries or problems"))
     end 
   end
 
@@ -67,9 +67,9 @@ end
 
   describe('#delete') do
   it("it will delete name by id") do 
-    definition = Definition.new({:definition => "easy", :id => 1})
+    definition = Definition.new({:definition => "without difficulty or effort", :id => 1})
     definition.save()
-    definition1 = Definition.new({:definition => "hard", :id => 2})
+    definition1 = Definition.new({:definition => "free from worries or problems", :id => 2})
     definition.save()
     definition.delete()
     expect(Definition.all).to(eq([definition1]))
