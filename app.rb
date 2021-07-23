9997f5cb2185da968effd8fa97965f600e98d68f
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-23 14:50:22 -0500

    WOP: adding routes

--- app.rb
+++ app.rb
@@ -5,8 +5,26 @@ require('./lib/definitions')
 require('pry')
 also_reload('lib/**/*.rb')
 
+get('/') do
+  @words = Word.all
+  erb(:words)
+end 
 
+get('/home') do
+  @words = Word.all
+  erb(:words)
+end 
 
+get('/home/new') do
+  erb(:new_word)
+end 
+
+post('/home') do 
+  word = Word.new(:word => paramswords_word)
+  word.save()
+  @word = word
+  erb(:words)
+end 
 
 
 
--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -2,3 +2,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('create an word path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('words_word', :with => 'easy')
+    click_on('click')
+    expect(page).to have_content('easy')
+  end
+end
