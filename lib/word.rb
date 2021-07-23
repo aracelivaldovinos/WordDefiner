f507c433a4f82f7fb41a7582a8be83ed03d28976
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-07-23 11:05:40 -0500

    create class word and initialize attributes with a Hash

--- lib/word.rb
+++ lib/word.rb
@@ -1,3 +1,13 @@
 require 'pry'
 
+class Word
+  @@words = {}
+  @@total_words = 0
+
+  def initialize(attributes)
+    @word = attributes.fetch(:word)
+    @id = attributes.fetch(:id) || @@total_words += 1
+  end 
+
+end 
 
